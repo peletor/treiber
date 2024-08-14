60d77eceb9de64349108cab90a97ce17527946ef
peletor <peletor@yandex.ru>
2024-08-14 10:16:56 +0300

    Refactor deque to two-way list

--- deque/deque.go
+++ deque/deque.go
@@ -20,29 +20,78 @@ func NewDeque() Deque {
 	return Deque{}
 }
 
-func (q *Deque) PushBack(value int) {
-	newItem := &dequeItem{value: value, next: nil}
+func (d *Deque) PushBack(value int) {
+	newItem := unsafe.Pointer(&dequeItem{value: value, next: nil})
 
 	for {
-		back := atomic.LoadPointer(&q.back)
-		newItem.prev = back
+		back := atomic.LoadPointer(&d.back)
+		(*dequeItem)(newItem).prev = back
 
-		if atomic.CompareAndSwapPointer(&q.back, back, unsafe.Pointer(newItem)) {
-			return
+		if back != nil {
+			// Deque is not empty. dequeItem = *q.back exist
+			next := atomic.LoadPointer(&(*dequeItem)(back).next)
+			// if d.back is not changed in other goroutine
+			if back == atomic.LoadPointer(&d.back) {
+				if next == nil {
+					if atomic.CompareAndSwapPointer(&(*dequeItem)(back).next, next, newItem) {
+						// try to move d.back
+						atomic.CompareAndSwapPointer(&d.back, back, newItem)
+						return
+					}
+				} else {
+					// try to fix d.back
+					atomic.CompareAndSwapPointer(&d.back, back, next)
+				}
+			}
+		} else {
+			// Deque is empty.
+
+			// if deque back is not changed in other goroutine
+			if back == atomic.LoadPointer(&d.back) {
+				if atomic.CompareAndSwapPointer(&d.back, back, newItem) {
+					// try to move deque front
+					atomic.CompareAndSwapPointer(&d.front, nil, newItem)
+					return
+				} else {
+					// try to fix deque front
+					atomic.CompareAndSwapPointer(&d.front, nil, newItem)
+				}
+			}
 		}
 	}
 }
 
-func (q *Deque) PopBack() (value int, ok bool) {
+func (d *Deque) PopBack() (value int, ok bool) {
 	for {
-		back := atomic.LoadPointer(&q.back)
+		back := atomic.LoadPointer(&d.back)
 		if back == nil {
+			// Deque is empty
 			return 0, false
 		}
 		prev := atomic.LoadPointer(&(*dequeItem)(back).prev)
 
-		if atomic.CompareAndSwapPointer(&q.back, back, prev) {
-			return (*dequeItem)(back).value, true
+		if back == atomic.LoadPointer(&d.back) {
+			// if deque has only one dequeItem
+			if prev == nil {
+				if atomic.CompareAndSwapPointer(&d.back, back, prev) {
+					// Try to move deque front
+					atomic.CompareAndSwapPointer(&d.front, back, nil)
+					return (*dequeItem)(back).value, true
+				}
+			} else {
+				// now prevItem.next == q.back
+				prevItemNext := atomic.LoadPointer(&(*dequeItem)(prev).next)
+				// try to make prevItem = last item (prevItem.next = nil)
+				if atomic.CompareAndSwapPointer(&(*dequeItem)(prev).next, prevItemNext, nil) {
+					// try to move d.back
+					atomic.CompareAndSwapPointer(&d.back, back, prev)
+					return (*dequeItem)(back).value, true
+				} else {
+					// try to fix d.back
+					atomic.CompareAndSwapPointer(&d.back, back, prev)
+				}
+
+			}
 		}
 	}
 }
