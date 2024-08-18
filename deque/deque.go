bd3f8a90c17f20a712368ddc9c0a2efc5c537bff
peletor <peletor@yandex.ru>
2024-08-18 20:00:30 +0300

    Add deque methods: PushFront and PopFront

--- deque/deque.go
+++ deque/deque.go
@@ -68,7 +68,7 @@ func (d *Deque) PushBack(value int) {
 func (d *Deque) PopBack() (value int, ok bool) {
 	for {
 		back := atomic.LoadPointer(&d.back)
-		//front := atomic.LoadPointer(&d.front)
+		front := atomic.LoadPointer(&d.front)
 		if back == nil {
 			// Deque is empty
 			return 0, false
@@ -82,7 +82,7 @@ func (d *Deque) PopBack() (value int, ok bool) {
 			if prev == nil {
 				if atomic.CompareAndSwapPointer(&d.back, back, nil) {
 					// Try to move deque front
-					//atomic.CompareAndSwapPointer(&d.front, front, nil)
+					atomic.CompareAndSwapPointer(&d.front, front, nil)
 
 					atomic.AddInt32(&d.counter2, 1)
 					return (*dequeItem)(back).value, true
@@ -99,6 +99,83 @@ func (d *Deque) PopBack() (value int, ok bool) {
 					} else {
 						// debug!
 						atomic.AddInt32(&d.counter2, 1)
+					}
+				}
+			}
+		}
+	}
+}
+
+func (d *Deque) PushFront(value int) {
+	newItem := unsafe.Pointer(&dequeItem{value: value, prev: nil})
+
+	for {
+		front := atomic.LoadPointer(&d.front)
+		(*dequeItem)(newItem).next = front
+
+		if front != nil {
+			// Deque is not empty. dequeItem = *q.front exist
+			prev := atomic.LoadPointer(&(*dequeItem)(front).prev)
+			// if d.front is not changed in other goroutine
+			if front == atomic.LoadPointer(&d.front) {
+				if prev == nil {
+					if atomic.CompareAndSwapPointer(&(*dequeItem)(front).prev, prev, newItem) {
+						// try to move d.front
+						atomic.CompareAndSwapPointer(&d.front, front, newItem)
+						return
+					}
+				} else {
+					// try to fix d.front
+					atomic.CompareAndSwapPointer(&d.front, front, prev)
+				}
+			}
+		} else {
+			// Deque is empty
+
+			// if d.front is not changed in other goroutine
+			if front == atomic.LoadPointer(&d.front) {
+				if atomic.CompareAndSwapPointer(&d.front, front, newItem) {
+					// try to move deque back
+					atomic.CompareAndSwapPointer(&d.back, nil, newItem)
+					return
+				} else {
+					atomic.CompareAndSwapPointer(&d.back, nil, newItem)
+				}
+			}
+		}
+	}
+}
+
+func (d *Deque) PopFront() (value int, ok bool) {
+	for {
+		front := atomic.LoadPointer(&d.front)
+		back := atomic.LoadPointer(&d.back)
+		if front == nil {
+			// Deque is empty
+			return 0, false
+		}
+
+		// Deque is not empty. dequeItem = *q.front exist
+		next := atomic.LoadPointer(&(*dequeItem)(front).next)
+
+		if front == atomic.LoadPointer(&d.front) {
+			// if deque has only one dequeItem
+			if next == nil {
+				if atomic.CompareAndSwapPointer(&d.front, front, nil) {
+					// Try to move deque back
+					atomic.CompareAndSwapPointer(&d.back, back, nil)
+
+					return (*dequeItem)(front).value, true
+				}
+			} else {
+				// now nextItem.prev == q.front
+				nextItemPrev := atomic.LoadPointer(&(*dequeItem)(next).prev)
+				// try to make nextItem = first item (nextItem.prev = nil)
+				if nextItemPrev != nil {
+					if atomic.CompareAndSwapPointer(&(*dequeItem)(next).prev, nextItemPrev, nil) {
+						// try to move d.front
+						atomic.CompareAndSwapPointer(&d.front, front, next)
+						return (*dequeItem)(front).value, true
 					}
 				}
 			}
--- deque/deque_test.go
+++ deque/deque_test.go
@@ -59,7 +59,6 @@ func TestPushBack(t *testing.T) {
 		deq.PushBack(value)
 		assert.Equal(t, value, (*dequeItem)(deq.back).value)
 	})
-
 }
 
 func TestPopBack(t *testing.T) {
@@ -208,5 +207,241 @@ func TestDequeConcurrencyPushBackPopBack(t *testing.T) {
 		val, ok := deq.PopBack()
 		t.Log("Last val:", val, ok, "Counter1", deq.counter1, "Counter2", deq.counter2)
 		assert.False(t, ok) // Queue must be empty
+	})
+}
+
+func TestPushFront(t *testing.T) {
+	const value = 10
+
+	t.Run("PushFront exist", func(t *testing.T) {
+		assert.NotPanics(t, func() {
+			deq := NewDeque()
+			deq.PushFront(value)
+		})
+	})
+
+	t.Run("PushFront do something", func(t *testing.T) {
+		deq := NewDeque()
+		assert.Nil(t, deq.front)
+		deq.PushFront(value)
+		assert.NotNil(t, deq.front)
+	})
+
+	t.Run("PushFront move deque front", func(t *testing.T) {
+		deq := NewDeque()
+		oldFront := deq.front
+		deq.PushFront(value)
+		newFront := deq.front
+		assert.NotEqual(t, oldFront, newFront)
+	})
+
+	t.Run("PushFront: front points to first item", func(t *testing.T) {
+		deq := NewDeque()
+		deq.PushFront(value)
+		assert.Nil(t, (*dequeItem)(deq.front).prev)
+	})
+
+	t.Run("PushFront: front points to item with correct value", func(t *testing.T) {
+		deq := NewDeque()
+		deq.PushFront(value)
+		assert.Equal(t, value, (*dequeItem)(deq.front).value)
+	})
+}
+
+func TestPushFrontPopBack(t *testing.T) {
+	const count = 100
+
+	t.Run("PushFrontPopBack several times", func(t *testing.T) {
+		deq := NewDeque()
+		for i := 0; i < count; i++ {
+			deq.PushFront(i)
+		}
+		for i := 0; i < count; i++ {
+			val, ok := deq.PopBack()
+			assert.True(t, ok)
+			assert.Equal(t, i, val)
+		}
+	})
+
+	t.Run("PushFrontPopBack several times together", func(t *testing.T) {
+		deq := NewDeque()
+		for i := 0; i < count; i++ {
+			deq.PushFront(i)
+			val, ok := deq.PopBack()
+			assert.True(t, ok)
+			assert.Equal(t, i, val)
+		}
+	})
+
+	t.Run("PushFrontPopBack: PopBack after several times", func(t *testing.T) {
+		deq := NewDeque()
+		for i := 0; i < count; i++ {
+			deq.PushFront(i)
+		}
+		for i := 0; i < count; i++ {
+			deq.PopBack()
+		}
+		val, ok := deq.PopBack()
+		assert.False(t, ok)
+		assert.Zero(t, val)
+	})
+
+	t.Run("PushFrontPopBack: PopBack after several times together", func(t *testing.T) {
+		deq := NewDeque()
+		for i := 0; i < count; i++ {
+			deq.PushFront(i)
+			deq.PopBack()
+		}
+		val, ok := deq.PopBack()
+		assert.False(t, ok)
+		assert.Zero(t, val)
+	})
+}
+
+func TestPopFront(t *testing.T) {
+	const value = 15
+
+	t.Run("PopFront exist", func(t *testing.T) {
+		assert.NotPanics(t, func() {
+			deq := NewDeque()
+			_, _ = deq.PopFront()
+		})
+	})
+
+	t.Run("PopFront on empty deque", func(t *testing.T) {
+		deq := NewDeque()
+		_, ok := deq.PopFront()
+		assert.False(t, ok)
+	})
+
+	t.Run("PopFront on no empty deque", func(t *testing.T) {
+		deq := NewDeque()
+		deq.PushFront(value)
+		_, ok := deq.PopFront()
+		assert.True(t, ok)
+	})
+
+	t.Run("PopFront do something", func(t *testing.T) {
+		deq := NewDeque()
+		deq.PushFront(value)
+		oldFront := deq.front
+		deq.PopFront()
+		newFront := deq.front
+		assert.NotEqual(t, oldFront, newFront)
+	})
+
+	t.Run("PopFront return correct value", func(t *testing.T) {
+		deq := NewDeque()
+		deq.PushFront(value)
+		val, _ := deq.PopFront()
+		assert.Equal(t, value, val)
+	})
+
+	t.Run("PopFront: front points to first item", func(t *testing.T) {
+		deq := NewDeque()
+		deq.PushFront(value)
+		deq.PushFront(value)
+		deq.PopFront()
+		assert.Nil(t, (*dequeItem)(deq.front).prev)
+	})
+}
+
+func TestPushFrontPopFront(t *testing.T) {
+	const count = 100
+
+	t.Run("PushFrontPopFront several times", func(t *testing.T) {
+		deq := NewDeque()
+		for i := 0; i < count; i++ {
+			deq.PushFront(i)
+		}
+		for i := 0; i < count; i++ {
+			val, ok := deq.PopFront()
+			assert.True(t, ok)
+			assert.Equal(t, count-i-1, val)
+		}
+	})
+
+	t.Run("PushFrontPopFront several times together", func(t *testing.T) {
+		deq := NewDeque()
+		for i := 0; i < count; i++ {
+			deq.PushFront(i)
+			val, ok := deq.PopFront()
+			assert.True(t, ok)
+			assert.Equal(t, i, val)
+		}
+	})
+
+	t.Run("PushFrontPopFront: PopFront after several times", func(t *testing.T) {
+		deq := NewDeque()
+		for i := 0; i < count; i++ {
+			deq.PushFront(i)
+		}
+		for i := 0; i < count; i++ {
+			deq.PopFront()
+		}
+		val, ok := deq.PopFront()
+		assert.False(t, ok)
+		assert.Zero(t, val)
+	})
+
+	t.Run("PushFrontPopFront: PopFront after several times together", func(t *testing.T) {
+		deq := NewDeque()
+		for i := 0; i < count; i++ {
+			deq.PushFront(i)
+			deq.PopFront()
+		}
+		val, ok := deq.PopFront()
+		assert.False(t, ok)
+		assert.Zero(t, val)
+	})
+}
+
+func TestPushBackPopFront(t *testing.T) {
+	const count = 100
+
+	t.Run("PushBackPopFront several times", func(t *testing.T) {
+		deq := NewDeque()
+		for i := 0; i < count; i++ {
+			deq.PushBack(i)
+		}
+		for i := 0; i < count; i++ {
+			val, ok := deq.PopFront()
+			assert.True(t, ok)
+			assert.Equal(t, i, val)
+		}
+	})
+
+	t.Run("PushBackPopFront several times together", func(t *testing.T) {
+		deq := NewDeque()
+		for i := 0; i < count; i++ {
+			deq.PushBack(i)
+			val, ok := deq.PopFront()
+			assert.True(t, ok)
+			assert.Equal(t, i, val)
+		}
+	})
+
+	t.Run("PushBackPopFront: PopFront after several times", func(t *testing.T) {
+		deq := NewDeque()
+		for i := 0; i < count; i++ {
+			deq.PushBack(i)
+		}
+		for i := 0; i < count; i++ {
+			deq.PopFront()
+		}
+		val, ok := deq.PopFront()
+		assert.False(t, ok)
+		assert.Zero(t, val)
+	})
+
+	t.Run("PushBackPopFront: PopFront after several times together", func(t *testing.T) {
+		deq := NewDeque()
+		for i := 0; i < count; i++ {
+			deq.PushBack(i)
+			deq.PopFront()
+		}
+		val, ok := deq.PopFront()
+		assert.False(t, ok)
+		assert.Zero(t, val)
 	})
 }
